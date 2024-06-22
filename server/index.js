5fcf43c39b57a1a014506f45ca8beaa5635d6c98
AnishGarg1 <anishgarg805@gmail.com>
2024-06-22 19:49:40 +0530

    server initial updates

--- server/index.js
+++ server/index.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const app = express();
+const database = require("./config/database");
+const dotenv = require("dotenv");
 
-const port = 4000;
+dotenv.config(); // importing all env variable
+
+// Connecting to Database
+database.dbConnect();
+
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
     console.log(`App is listening on port ${port}`);
